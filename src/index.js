263bece01d3267766cb524623ff5c84d84de5f6d
Ruslan Chernov <chernruslan@gmail.com>
2018-11-22 15:22:47 +0300

    add page swipe

--- src/index.js
+++ src/index.js
@@ -92,15 +92,94 @@ const parallax = document.querySelector('.slide__second-wrapper'),
     setValue = (val, mode) => {
         document.getElementById("slider").value = val;
         showValue(val, mode);
+    },
+
+    slideParalax = () => {
+        const windowYOffset = window.pageYOffset;
+    	parallax.style.top = (-windowYOffset * .5 + 400) + "px";
+        // console.log(windowYOffset, parallax.style.top);
     };
 
 document.addEventListener('DOMContentLoaded', function(){
   setValue(0,false);
 })
 
-window.onscroll = function() {
-	const windowYOffset = window.pageYOffset;
-	parallax.style.top = (-windowYOffset * .5 + 400) + "px";
+
+
+window.onscroll = slideParalax;
+
+const _C = document.querySelector('.slide'),
+      N = 3;
+
+let i = 0, y0 = null, locked = false, dif = null, position = 0;
+
+function unify(e) {	return e.changedTouches ? e.changedTouches[0] : e };
+
+function lock(e) {
+    y0 = unify(e).clientY;
+    // position = e.offsetTop;
+    console.log(e);
+	// _C.classList.toggle('smooth', !(locked = true))
+    console.log('touchstart');
 };
+
+function drag(e) {
+    console.log('move');
+	// e.preventDefault();
+// // slideParalax();
+    console.log(`${Math.round(unify(e).clientY- y0)}px`);
+    // dif = Math.round(unify(e).clientY- y0) > 0 ?  : 768;
+    if(Math.round(unify(e).clientY- y0) > 50) {
+        dif = -768;
+    }
+    else if (Math.round(unify(e).clientY- y0) < -50 ) {
+        dif = 768;
+    }
+    else {
+        dif = 0;
+    }
+// 	// if(locked)
+// 		// _C.style.setProperty('--ty', `${Math.round(unify(e).clientY- y0)}px`)
+};
+
+function move(e) {
+    console.log('touchend');
+    let dy = unify(e).clientY - y0, s = Math.sign(dy);
+    console.log(position);
+
+    window.scrollTo({
+        top: position+dif,
+        behavior: 'smooth'
+    });
+
+    position = position + dif;
+  // if(locked) {
+  //   let dy = unify(e).clientY - y0, s = Math.sign(dy);
+  //   console.log(dy);
+  //   //
+  //   // if((i > 0 || s < 0) && (i < N - 1 || s > 0))
+  //   //   _C.style.setProperty('--i', i -= s);
+  //   // _C.style.setProperty('--ty', '0px');
+  //   // _C.classList.toggle('smooth', !(locked = false));
+  //   // y0 = null;
+  //
+  //   window.scrollTo({
+  //     top: dy+768,
+  //     behavior: 'smooth'
+  //   });
+  // }
+
+};
+
+_C.style.setProperty('--n', N);
+
+_C.addEventListener('mousedown', lock, false);
+_C.addEventListener('touchstart', lock, false);
+
+_C.addEventListener('mousemove', drag, false);
+_C.addEventListener('touchmove', drag, false);
+
+_C.addEventListener('mouseup', move, false);
+_C.addEventListener('touchend', move, false);
 
 exports.showValue = showValue;
