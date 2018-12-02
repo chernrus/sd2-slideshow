d3a638e4e61daf4a5cfd4769c82334f2a0385f6c
Ruslan Chernov <chernruslan@gmail.com>
2018-12-02 21:18:13 +0300

    add swipe module

--- src/swipe.js
+++ src/swipe.js
@@ -1,26 +1,28 @@
-let y0 = null,
-    dif = null,
-    position = 0;
 
-const _C = document.querySelector('.slide'),
-     N = 3,
-     pageDownArrowContainer = document.querySelector('.slide__page-down'),
+var swipe = (function() {
+    'use strict';
 
-    unify = (e) => {	return e.changedTouches ? e.changedTouches[0] : e },
+    let y0 = null,
+        dif = null,
+        position = 0;
+
+    const _C = document.querySelector('.slide'),
+         N = 3,
+         pageDownArrowContainer = document.querySelector('.slide__page-down');
+
+    function unify(e) {	return e.changedTouches ? e.changedTouches[0] : e; };
 
     /**
      * getPosition - get current position
      * @param {object} e event element
      */
-    getPosition = (e) => {
-        y0 = unify(e).clientY;
-    },
+    function getPosition(e) { y0 = unify(e).clientY; };
 
-   /**
-    * drag - event on touch or mousedown
-    * @param {object} e event element
-    */
-    drag = (e) => {
+    /**
+     * drag - event on touch or mousedown
+     * @param {object} e event element
+     */
+    function drag(e) {
         const touchTrack = Math.round(unify(e).clientY- y0);
         if(touchTrack > 40 && position > 0) {
             dif = -768;
@@ -31,52 +33,52 @@ const _C = document.querySelector('.slide'),
         else {
             dif = 0;
         }
-    },
+    };
 
     /**
      * move - move slide on next/prev page
      * @param {object} e event element
      */
-    move = (e) => {
+    function move(e) {
         if (dif != 0 && e.target.nodeName != 'BUTTON') {
             position = Math.abs(position + dif);
             goTo(null, position);
         }
-    },
+    };
 
     /**
      * hidePageDownArrow - hide block when last page
      */
-    hidePageDownArrow = () => {
+    function hidePageDownArrow() {
         pageDownArrowContainer.style.display = 'none';
-    },
+    };
 
     /**
      * showPageDownArrow - show block when non last page
      */
-    showPageDownArrow = () => {
+    function showPageDownArrow() {
         pageDownArrowContainer.style.display = 'block';
-    },
+    };
 
     /**
      * setActiveButton - change buttons color
      * @param {object} index - current page index
      */
-    setActiveButton = (index) => {
+    function setActiveButton(index) {
         const navButtons = document.querySelectorAll(".slide__navigation-button");
 
         navButtons.forEach((btn) => {
             btn.classList.remove('active');
         });
         navButtons[index].classList.add('active');
-    },
+    };
 
     /**
      * goTo - got to page by index or position
      * @param {number} index - current page index
      * @param {number} pos - position in px
      */
-    goTo = (index, pos) => {
+    function goTo(index, pos) {
 
         position = pos || 768 * index;
         window.scrollTo({
@@ -94,12 +96,15 @@ const _C = document.querySelector('.slide'),
         setActiveButton(index);
     };
 
-_C.addEventListener('touchstart', getPosition, false);
+    _C.addEventListener('touchstart', getPosition, false);
 
-_C.addEventListener('touchmove', drag, false);
+    _C.addEventListener('touchmove', drag, false);
 
-_C.addEventListener('touchend', move, false);
+    _C.addEventListener('touchend', move, false);
 
-goTo(0);
+    return {goTo: goTo}
+}());
 
-exports.goTo = goTo;
+swipe.goTo(0);
+
+exports.goTo = swipe.goTo;
